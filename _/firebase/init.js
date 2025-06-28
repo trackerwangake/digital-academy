e3bd310b4c039357e3bcd56aceb8d6f6862457b9
trackerwangake <promoticoads@gmail.com>
2025-06-29 00:58:39 +0300

    Create init.js

---
+++ _/firebase/init.js
@@ -0,0 +1,10 @@
+// Only if you're not using /__/firebase/init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDL0XCaXIB47WGlt8L9R1uGd3JBLs2oxOk",
+  authDomain: "wanga-digital-academy.firebaseapp.com",
+  projectId: "wanga-digital-academy",
+  storageBucket: "wanga-digital-academy.appspot.com",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+};
+firebase.initializeApp(firebaseConfig);
